cf60c697433e1290dc3a9ac2eb5deb50a9508e89
kl1650fm <loveland0227@gmail.com>
2025-08-31 19:19:30 -0500

    Add files via upload

---
+++ Week 1.py
@@ -0,0 +1,41 @@
+# This is wrtten by Tina Kim
+
+First_Name = "Tina"
+Last_Name = "Kim"
+Full_Name = "Tina Kim"
+Student_ID = 14865311
+Issue_date = 8/16/2021
+Exp_date = 8/16/2026
+Gender = "Female"
+University = "Minnesota State University, Mankato"
+
+#Terminal input
+Fisrt_Name = input("Please enter your first name")
+Last_Name = input("Please enter your last name")
+Full_Name = input("Please enter your full name")
+Student_ID = input("Please enter your student ID")
+Issue_date = input("Please enter your issue date")
+Exp_date = input("Please enter your expiration date")
+Gender = input("Please enter your gender")
+University = input("Please enter your university")
+
+""" 
+String - word/letters
+Integer - positive/negative
+Float - decimal number
+double - larger decimal number
+Boolean - true/false
+"""
+
+# Print
+print("First name is : "+ First_Name +" Last Name is : "+ Last_Name +" Full Name is : "+ Full_Name +"")
+
+
+# Basic I/O
+First_Name = "Tina"
+Last_Name = "Kim"
+
+First_Name = input("Please enter your first name")
+Last_Name = input("Please enter your last name")
+
+print("Hello" "+ First_Name + Last_Name +" "how are you?")
